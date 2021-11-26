defc332d862df2405b6b598ab015d3d573be4232
Bipin Khatiwada <bipinkh91@gmail.com>
2021-11-26 22:59:37 +0200

    Complete part 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,22 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import axios from "axios";
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', number: '040-123456', id: 1},
-        {name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
-        {name: 'Dan Abramov', number: '12-43-234345', id: 3},
-        {name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [filter, setFilter] = useState('')
 
+    // use effect hook
+    useEffect( () =>{
+        axios.get("http://localhost:3001/persons")
+            .then( response => setPersons(response.data) )
+        }, [])
+
+    // event handlers
     const addName = (event) => {
         event.preventDefault()
         if (persons.filter(p => p.name === newName).length > 0) {
@@ -29,8 +32,10 @@ const App = () => {
     const handleNewNumberChange = (event) => setNewNumber(event.target.value)
     const handleFilterChange = (event) => setFilter(event.target.value)
 
+    // processing the state to get useful data for display
     const personsToShow = persons.filter(p => p.name.toLowerCase().startsWith(filter.toLowerCase()))
 
+    // return
     return (
         <div>
             <h2>Phonebook</h2>
