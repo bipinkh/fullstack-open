ce1da5b406245e050386eda05416b3690d4e233e
Bipin Khatiwada <bipinkh91@gmail.com>
2022-01-09 12:53:40 +0200

    Use sample blogs for test

--- part4/blogs/tests/favorites.test.js
+++ part4/blogs/tests/favorites.test.js
@@ -1,19 +1,7 @@
 const listHelper = require('../utils/list_helper')
-const Blog = require('../models/blog')
+const sampleBlogs = require('../resources/sample_blogs')
 
 describe( 'favorite among ', () => {
-
-    const listWithOneBlog = [
-        { _id: '5a422aa71b54a676234d17f8', "title": "Blog 1", "author": "Bipin K", "url": "google.com",  likes: 20, __v: 0 }
-    ]
-
-    const listWithMultipleBlogs = [
-        { _id: '5a422aa71b54a676234d17f8', "title": "Blog 1", "author": "Bipin K", "url": "google.com",  likes: 20, __v: 0  },
-        { _id: '5a422aa71b54a676234d17f9', "title": "Blog 1", "author": "Bipin K", "url": "google.com",  likes: 43, __v: 0  },
-        { _id: '5a422aa71b54a676234d17f0', "title": "Blog 1", "author": "Bipin K", "url": "google.com",  likes: 12, __v: 0  },
-        { _id: '5a422aa71b54a676234d17f1', "title": "Blog 1", "author": "Bipin K", "url": "google.com",  likes: 9, __v: 0  },
-        { _id: '5a422aa71b54a676234d17f2', "title": "Blog 1", "author": "Bipin K", "url": "google.com",  likes: 756, __v: 0  }
-    ]
 
     test('empty list of blogs is None', () => {
         const result = listHelper.favoriteBlog( [] )
@@ -21,13 +9,13 @@ describe( 'favorite among ', () => {
     } )
 
     test('singleton list of blogs is itself', () =>{
-        const result = listHelper.favoriteBlog(listWithOneBlog)
-        expect(result).toEqual(listWithOneBlog[0])
+        const result = listHelper.favoriteBlog( sampleBlogs.blogs.slice(0,1) )
+        expect(result).toEqual( sampleBlogs.blogs[0] )
     })
 
     test(' list of blogs is the one with highest likes', () =>{
-        const result = listHelper.favoriteBlog(listWithMultipleBlogs)
-        expect(result).toEqual(listWithMultipleBlogs[4])
+        const result = listHelper.favoriteBlog( sampleBlogs.blogs )
+        expect(result).toEqual( sampleBlogs.blogs[2] )
     })
 
 })
--- part4/blogs/tests/likes.test.js
+++ part4/blogs/tests/likes.test.js
@@ -1,19 +1,7 @@
 const listHelper = require('../utils/list_helper')
-const Blog = require('../models/blog')
+const sampleBlogs = require('../resources/sample_blogs')
 
 describe( 'total likes', () => {
-
-    const listWithOneBlog = [
-        { _id: '5a422aa71b54a676234d17f8', "title": "Blog 1", "author": "Bipin K", "url": "google.com",  likes: 20, __v: 0 }
-    ]
-
-    const listWithMultipleBlogs = [
-        { _id: '5a422aa71b54a676234d17f8', "title": "Blog 1", "author": "Bipin K", "url": "google.com",  likes: 20, __v: 0  },
-        { _id: '5a422aa71b54a676234d17f9', "title": "Blog 1", "author": "Bipin K", "url": "google.com",  likes: 43, __v: 0  },
-        { _id: '5a422aa71b54a676234d17f0', "title": "Blog 1", "author": "Bipin K", "url": "google.com",  likes: 12, __v: 0  },
-        { _id: '5a422aa71b54a676234d17f1', "title": "Blog 1", "author": "Bipin K", "url": "google.com",  likes: 9, __v: 0  },
-        { _id: '5a422aa71b54a676234d17f2', "title": "Blog 1", "author": "Bipin K", "url": "google.com",  likes: 756, __v: 0  }
-    ]
 
     test('of empty list is zero', () => {
         const result = listHelper.countLikes( [] )
@@ -21,13 +9,13 @@ describe( 'total likes', () => {
     } )
 
     test('when list has only one blog equals the likes of that', () => {
-        const result = listHelper.countLikes(listWithOneBlog)
-        expect(result).toBe(20)
+        const result = listHelper.countLikes( sampleBlogs.blogs.slice(0,1) )
+        expect(result).toBe( 7 )
     } )
 
     test('of a bigger list is calculated right', () => {
-        const result = listHelper.countLikes(listWithMultipleBlogs)
-        expect(result).toBe( 20+43+12+9+756 )
+        const result = listHelper.countLikes( sampleBlogs.blogs )
+        expect(result).toBe( 36 )
     } )
 
 } )
