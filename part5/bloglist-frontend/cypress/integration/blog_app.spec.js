d8d8df630208a9e1b1f9ef9ce272367481b6a509
Bipin Khatiwada <bipinkh91@gmail.com>
2022-01-11 01:44:45 +0200

    Complete part 5.18

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -2,13 +2,38 @@ describe('Blog app', () => {
 
   beforeEach(function (){
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      username: 'bipin',
+      password: 'sekretp@ss',
+      name: 'bipin k'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:3000')
   })
 
-  it('displays the login form by default', () => {
+  it('login form is shown', () => {
     cy.contains('login')
     cy.contains('username')
     cy.contains('password')
   })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('bipin')
+      cy.get('#password').type('sekretp@ss')
+      cy.get('#login-button').click()
+      cy.contains('blogs')
+      cy.contains('bipin logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('bipin')
+      cy.get('#password').type('incorrrectPw@')
+      cy.get('#login-button').click()
+      cy.get('.error').contains('Wrong username or password')
+    })
+  })
+
+
 
 })
