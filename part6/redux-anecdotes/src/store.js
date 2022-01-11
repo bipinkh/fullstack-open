525f84ec45a1c599086356f03eb05969fb41f8e0
Bipin Khatiwada <bipinkh91@gmail.com>
2022-01-11 13:42:12 +0200

    Complete part 6.9

---
+++ part6/redux-anecdotes/src/store.js
@@ -0,0 +1,14 @@
+import anecdoteReducer from './reducers/anecdoteReducer'
+import {createStore, combineReducers} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+
+const reducer = combineReducers({
+    anecdotes: anecdoteReducer
+})
+
+const store = createStore(
+    reducer,
+    composeWithDevTools()
+)
+
+export default store
